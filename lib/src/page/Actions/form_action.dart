8e44f808ed8fe0aa6a8dc223cdeb3ae68c20b6ba
Christian Zavaleta <christianzavaleta20@gmail.com>
2022-08-09 11:20:15 -0500

    mod

--- lib/src/page/Actions/form_action.dart
+++ lib/src/page/Actions/form_action.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:marca_loreto/src/api/Form%20Action/form_action_api.dart';
+import 'package:marca_loreto/src/page/change_language.dart';
 import 'package:marca_loreto/src/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class FormAction extends StatefulWidget {
   const FormAction({Key? key}) : super(key: key);
@@ -18,6 +20,7 @@ class _FormActionState extends State<FormAction> {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ControllerLanguage>(context, listen: false);
     return Material(
       color: Colors.black.withOpacity(.1),
       child: Stack(
@@ -52,124 +55,135 @@ class _FormActionState extends State<FormAction> {
                 horizontal: ScreenUtil().setWidth(24),
                 vertical: ScreenUtil().setHeight(5),
               ),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: ScreenUtil().setHeight(60),
-                      child: Image.asset('assets/img/logos/logo.png'),
-                    ),
-                    Text(
-                      'Loreto tiene algo que contarte.\nDescubre de qué se trata, ingresando tus datos.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w500,
-                        fontSize: ScreenUtil().setSp(16),
-                        fontStyle: FontStyle.normal,
-                      ),
-                    ),
-                    SizedBox(
-                      height: ScreenUtil().setHeight(100),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: ScreenUtil().setHeight(10),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: ScreenUtil().setWidth(8),
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.green),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: TextField(
-                        controller: _nombreController,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: ScreenUtil().setSp(16),
-                          fontWeight: FontWeight.w400,
-                        ),
-                        decoration: InputDecoration(
-                          label: Text(
-                            'Nombre',
+              child: ValueListenableBuilder(
+                  valueListenable: provider.activate,
+                  builder: (BuildContext context, int data, Widget? child) {
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: ScreenUtil().setHeight(60),
+                            child: Image.asset('assets/img/logos/logo.png'),
+                          ),
+                          Text(
+                            (provider.activateS.value == 1)
+                                ? 'Loreto has something to tell you.\nFind out what it is, sign up now'
+                                : 'Loreto tiene algo que contarte.\nDescubre de qué se trata, ingresando tus datos.',
+                            textAlign: TextAlign.center,
                             style: TextStyle(
-                              fontSize: ScreenUtil().setSp(14),
-                              fontWeight: FontWeight.w400,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: ScreenUtil().setSp(16),
+                              fontStyle: FontStyle.normal,
                             ),
                           ),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: ScreenUtil().setHeight(10),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: ScreenUtil().setWidth(8),
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.green),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: TextField(
-                        controller: _emailController,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: ScreenUtil().setSp(16),
-                          fontWeight: FontWeight.w400,
-                        ),
-                        decoration: InputDecoration(
-                          label: Text(
-                            'Email',
-                            style: TextStyle(
-                              fontSize: ScreenUtil().setSp(14),
-                              fontWeight: FontWeight.w400,
+                          SizedBox(
+                            height: ScreenUtil().setHeight(100),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: ScreenUtil().setHeight(10),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: ScreenUtil().setWidth(8),
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.green),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: TextField(
+                              controller: _nombreController,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: ScreenUtil().setSp(16),
+                                fontWeight: FontWeight.w400,
+                              ),
+                              decoration: InputDecoration(
+                                label: Text(
+                                  (provider.activateS.value == 1) ? 'Name' : 'Nombre',
+                                  style: TextStyle(
+                                    fontSize: ScreenUtil().setSp(14),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                border: InputBorder.none,
+                              ),
                             ),
                           ),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: ScreenUtil().setHeight(10)),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (_nombreController.value.text.isNotEmpty) {
-                          if (_emailController.value.text.isNotEmpty) {
-                            _controller.changeCargando(true);
-                            final _api = FormActionApi();
-                            final res = await _api.sendDatos(_nombreController.value.text.trim(), _emailController.value.text.trim());
-                            if (res == 1) {
-                              showToast2('Datos enviados correctamente', Colors.green);
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: ScreenUtil().setHeight(10),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: ScreenUtil().setWidth(8),
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.green),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: TextField(
+                              controller: _emailController,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: ScreenUtil().setSp(16),
+                                fontWeight: FontWeight.w400,
+                              ),
+                              decoration: InputDecoration(
+                                label: Text(
+                                  'Email',
+                                  style: TextStyle(
+                                    fontSize: ScreenUtil().setSp(14),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: ScreenUtil().setHeight(10)),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (_nombreController.value.text.isNotEmpty) {
+                                if (_emailController.value.text.isNotEmpty) {
+                                  _controller.changeCargando(true);
+                                  final _api = FormActionApi();
+                                  final res = await _api.sendDatos(_nombreController.value.text.trim(), _emailController.value.text.trim());
+                                  if (res == 1) {
+                                    showToast2(
+                                        (provider.activateS.value == 1) ? 'Data sent successfully' : 'Datos enviados correctamente', Colors.green);
+                                    Navigator.pop(context);
+                                  } else {
+                                    showToast2(
+                                        (provider.activateS.value == 1)
+                                            ? 'An error occurred, please try again'
+                                            : 'Ocurrió un error, inténtelo nuevamente',
+                                        Colors.redAccent);
+                                  }
+                                  _controller.changeCargando(false);
+                                } else {
+                                  showToast2((provider.activateS.value == 1) ? 'Email required' : 'Email requerido', Colors.redAccent);
+                                }
+                              } else {
+                                showToast2((provider.activateS.value == 1) ? 'Name required' : 'Nombre requerido', Colors.redAccent);
+                              }
+                            },
+                            child: Text((provider.activateS.value == 1) ? 'Send' : 'Enviar'),
+                          ),
+                          TextButton(
+                            onPressed: () {
                               Navigator.pop(context);
-                            } else {
-                              showToast2('Ocurrió un error, inténtelo nuevamente', Colors.redAccent);
-                            }
-                            _controller.changeCargando(false);
-                          } else {
-                            showToast2('Email requerido', Colors.redAccent);
-                          }
-                        } else {
-                          showToast2('Nombre requerido', Colors.redAccent);
-                        }
-                      },
-                      child: Text('Enviar'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        'Volver',
-                        style: TextStyle(
-                          color: Colors.redAccent,
-                        ),
+                            },
+                            child: Text(
+                              (provider.activateS.value == 1) ? 'Return' : 'Volver',
+                              style: TextStyle(
+                                color: Colors.redAccent,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
+                    );
+                  }),
             ),
           ),
 
